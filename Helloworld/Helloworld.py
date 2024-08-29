447e56052e5527f6fda7920d9a08e75fbcd027ce
hello <1825525673@qq.com>
2024-08-30 00:22:19 +0800

    第一个程序

--- Helloworld/Helloworld.py
+++ Helloworld/Helloworld.py
@@ -1,3 +1,8 @@
-print("Hello world!")
+message = "Hello world!"#定义变量messge，并对变量进行赋值操作；
+print(message)
+print(message.title())#以首字母大写的方式显示每个单词；
+print(message.upper())#将所有字符串修改为全部大写
+print(message.lower())#将所有字符串修改为全部小写
+
 a=2
-print(f"n={a}")
+print(f"n={a}")   #f-string字符串
