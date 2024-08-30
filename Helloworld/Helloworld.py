e5b0a89d756cf45965ffbecd3a117338fd8e680e
hello <1825525673@qq.com>
2024-08-30 19:44:28 +0800

    第一个程序

--- Helloworld/Helloworld.py
+++ Helloworld/Helloworld.py
@@ -4,5 +4,10 @@ print(message.title())#以首字母大写的方式显示每个单词；
 print(message.upper())#将所有字符串修改为全部大写
 print(message.lower())#将所有字符串修改为全部小写
 
-a=2
-print(f"n={a}")   #f-string字符串
+a = 10
+b = 20
+print(f"""
+The value of a is {a}
+The value of b is {b}
+The sum of a and b is {a + b}
+""")
