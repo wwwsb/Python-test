2e9011da03b741303ea7a625eb92a6e2fd5e5ab8
hello <1825525673@qq.com>
2024-08-31 18:59:30 +0800

    第一个程序

---
+++ Python-Demo/HelloWorld/Helloworld.py
@@ -0,0 +1,15 @@
+message = "Hello world!"#定义变量messge，并对变量进行赋值操作；
+print(message)
+print(message.title())#以首字母大写的方式显示每个单词；
+print(message.upper())#将所有字符串修改为全部大写
+print(message.lower())#将所有字符串修改为全部小写
+
+a = 10
+b = 20
+print(f"""
+The value of a is {a} 
+The value of b is {b}
+The sum of a and b is {a + b}
+""")
+
+
---
+++ Python-Demo/变量/变量.py
@@ -0,0 +1,8 @@
+x = 5
+y = 6
+print(x+y)
+
+age = 25
+print(f"我的年龄为{age}")
+# print("你好"+age)
+print("我的年龄"+str(age))
