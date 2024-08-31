05492826b33725085d5a9cb6f9ec55ae11dadc18
hello <1825525673@qq.com>
2024-08-31 20:30:14 +0800

    添加代码

---
+++ Python-Demo/输入输出/输入输出.py
@@ -0,0 +1,16 @@
+#input获得使用者的输入
+name = input("输入名字：")
+print(f"你的名字是：{name}")
+
+
+#填词游戏
+print("今天天气怎么样：")
+adj_1 = input("请输入形容词:")
+print(f"今天天气{adj_1}")
+
+
+#计算矩形面积
+length = float(input("矩形长度："))
+width = float(input("矩形宽度："))
+print(f"矩形面积为{length}*{width}=",length*width)
+
