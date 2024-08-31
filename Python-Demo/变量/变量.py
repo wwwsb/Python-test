28cc73e4f1ef3565c5295f5e2aba92e29abe09da
hello <1825525673@qq.com>
2024-08-31 19:09:14 +0800

    第一个程序

--- Python-Demo/变量/变量.py
+++ Python-Demo/变量/变量.py
@@ -1,8 +1,26 @@
-x = 5
-y = 6
-print(x+y)
 
+#int：整数
 age = 25
 print(f"我的年龄为{age}")
 # print("你好"+age)
 print("我的年龄"+str(age))
+
+#float：浮点数
+score = 98.2
+print(f"成绩为{score}")
+
+#string：字符串
+name = "peter"
+print(f"我的名字为{name}")
+
+#布尔值：true或者flase
+
+is_true = True
+print(f"在线上吗？{is_true}")
+
+
+#判断类别type()
+print(type(is_true))
+
+
+
