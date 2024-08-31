3431fe2e540decb83caae02ec87d7f2741323b20
hello <1825525673@qq.com>
2024-08-31 21:12:56 +0800

    添加代码

---
+++ Python-Demo/算数运算符/算数符.py
@@ -0,0 +1,13 @@
+#python中的数学
+apple_num = int(input('苹果数量：'))
+apple_num += 1
+print(apple_num)
+
+apple_num *= 2
+print(apple_num)
+
+apple_num = apple_num ** 3 #三次方
+print(apple_num)
+
+num = 10
+print(num%3)#除余10/3=3>>>1
